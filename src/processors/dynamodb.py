6027cefb9b039a10798306afe63908acee724619
ThomasWaite03 <thomas.waite03@gmail.com>
2022-10-25 17:52:51 -0600

    Implemented the S3 request processor.

--- src/processors/dynamodb.py
+++ src/processors/dynamodb.py
@@ -1,5 +1,9 @@
+from .processor import RequestProcessor
 
 
-class DynamoDBRequestProcessor:
+class DynamoDBRequestProcessor(RequestProcessor):
     def __init__(self, table):
         self.table = table
+
+    def process(self, widget_request):
+        print()
--- src/processors/s3.py
+++ src/processors/s3.py
@@ -1,9 +1,30 @@
+import boto3
+
 from .processor import RequestProcessor
 
 
 class S3RequestProcessor(RequestProcessor):
     def __init__(self, bucket):
-        self.bucket = bucket
+        self._bucket = bucket
+        self._s3_client = boto3.client('s3')
 
     def process(self, widget_request):
-        print()
+        if widget_request.get_type() == 'create':
+            self.__create_widget(widget_request)
+        elif widget_request.get_type() == 'update':
+            self.__create_widget(widget_request)
+        elif widget_request.get_type() == 'delete':
+            self.__delete_widget(widget_request)
+        else:
+            print('WARNING: Invalid request type.')
+
+    def __create_widget(self, widget_request):
+        widget_key = f'widgets/{widget_request.get_owner()}/{widget_request.get_widget_id()}'
+        data = widget_request.get_widget().to_string().encode()
+        self._s3_client.put_object(Bucket=self._bucket, Key=widget_key, Body=data)
+
+    def __update_widget(self, widget_request):
+        pass
+
+    def __delete_widget(self, widget_request):
+        pass
