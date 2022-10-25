a7c3296d0065693240b914c4a5c40968894a7dc4
ThomasWaite03 <thomas.waite03@gmail.com>
2022-10-25 10:54:27 -0600

    Created the Widget and Attribute classes

---
+++ src/widgets/attribute.py
@@ -0,0 +1,12 @@
+
+
+class Attribute:
+    def __init__(self, name, value):
+        self._name = name
+        self._value = value
+
+    def get_name(self):
+        return self._name
+
+    def get_value(self):
+        return self._value
---
+++ src/widgets/widget.py
@@ -0,0 +1,35 @@
+import json
+
+from attribute import Attribute
+
+
+class Widget:
+    def __init__(self, widget_id, owner, label, description, widget_string):
+        self._id = widget_id
+        self._owner = owner
+        self._label = label
+        self._description = description
+        self._widget_string = widget_string
+
+        # Create attribute objects for the "other attributes"
+        self._other_attributes = []
+        for other_attr in json.loads(widget_string):
+            self._other_attributes += [Attribute(other_attr['name'], other_attr['value'])]
+
+    def get_id(self):
+        return self._id
+
+    def get_label(self):
+        return self._label
+
+    def get_description(self):
+        return self._description
+
+    def get_owner(self):
+        return self._owner
+
+    def get_other_attributes(self):
+        return self._other_attributes
+
+    def to_string(self):
+        return self._widget_string
