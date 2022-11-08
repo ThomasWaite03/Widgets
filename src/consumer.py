74729cc2d24146c22d102c913c2671ddd0ba5522
ThomasWaite03 <thomas.waite03@gmail.com>
2022-11-08 12:45:55 -0700

    Made the logger print to the console in addition to saving to a file.

--- src/consumer.py
+++ src/consumer.py
@@ -12,6 +12,13 @@ def main():
     format_str = '%(asctime)s - %(message)s'
     logging.basicConfig(filename=log_filename, filemode='w', format=format_str, level=logging.INFO)
 
+    # These 3 lines are from the logging module documentation and makes the logger print to the console
+    # Source: https://docs.python.org/3/howto/logging-cookbook.html#logging-to-multiple-destinations
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    logging.getLogger('').addHandler(console)
+
+    # Get the request retriever and request processor per the user's command line args
     request_retriever, request_processor = get_strategies()
     start_time = time.time()
     max_seconds = 30
