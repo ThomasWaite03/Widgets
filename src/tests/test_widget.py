937e0d72e2a4cd48d308d2c293024db1fd9272a0
ThomasWaite03 <thomas.waite03@gmail.com>
2022-11-08 17:37:51 -0700

    Added more unit tests for the Widget class.

--- src/tests/test_widget.py
+++ src/tests/test_widget.py
@@ -2,6 +2,7 @@ import unittest
 import json
 
 from widgets.widget import Widget
+from widgets.attribute import Attribute
 
 
 class WidgetTestCase(unittest.TestCase):
@@ -33,8 +34,18 @@ class WidgetTestCase(unittest.TestCase):
     def test_get_label(self):
         self.assertEqual(self.widget.get_label(), self.label)
 
+    def test_set_label(self):
+        new_label = 'new label!'
+        self.widget.set_label(new_label)
+        self.assertEqual(self.widget.get_label(), new_label)
+
     def test_get_description(self):
         self.assertEqual(self.widget.get_description(), self.description)
+
+    def test_set_description(self):
+        new_description = 'new description!'
+        self.widget.set_description(new_description)
+        self.assertEqual(self.widget.get_description(), new_description)
 
     def test_to_string(self):
         self.assertEqual(self.widget.to_string(), self.widget_string)
@@ -47,6 +58,26 @@ class WidgetTestCase(unittest.TestCase):
             self.assertEqual(other_attributes[0].get_value(), 'john')
             self.assertEqual(other_attributes[1].get_name(), 'state')
             self.assertEqual(other_attributes[1].get_value(), 'utah')
+
+    def test_add_other_attribute(self):
+        new_attribute = Attribute('height', '6 ft')
+        self.widget.add_other_attribute(new_attribute)
+
+        attribute_added = False
+        for attr in self.widget.get_other_attributes():
+            if attr.get_name() == new_attribute.get_name() and attr.get_value() == new_attribute.get_value():
+                attribute_added = True
+        self.assertTrue(attribute_added)
+
+    def test_delete_other_attribute(self):
+        attribute_name_to_delete = 'state'
+        self.widget.delete_other_attribute(attribute_name_to_delete)
+
+        attribute_present = False
+        for attr in self.widget.get_other_attributes():
+            if attr.get_name() == attribute_name_to_delete:
+                attribute_present = True
+        self.assertFalse(attribute_present)
 
 
 if __name__ == '__main__':
