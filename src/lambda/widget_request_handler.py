e2a5344e36542d6ea9851da6a13379ee4d996c63
ThomasWaite03 <thomas.waite03@gmail.com>
2022-11-20 13:23:47 -0700

    Modified the enqueue_request method and the event handler.

--- src/lambda/widget_request_handler.py
+++ src/lambda/widget_request_handler.py
@@ -14,10 +14,7 @@ logging.basicConfig(format=format_str, level=logging.INFO)
 
 def widget_request_handler(event, context):
     if is_valid_request(event):
-        enqueue_request(event['body'])
-        return {
-            "statusCode": 200
-        }
+        return enqueue_request(event['body'])
     else:
         return create_error_message({
             "status": 400,
@@ -44,8 +41,16 @@ def enqueue_request(request_str):
         queue_url = response["QueueUrl"]
         client.send_message(QueueUrl=queue_url, MessageBody=request_str)
         logging.info('Successfully sent widget request to SQS queue.')
+        return {
+            "statusCode": 200
+        }
     except botocore.exceptions.ClientError:
         logging.error('Failed to enqueue request.')
+        return create_error_message({
+            "status": 500,
+            "error": "Internal Server Error",
+            "msg": "Your widget request was not able to be enqueued into the SQS queue."
+        })
 
 
 def is_valid_request(event):
